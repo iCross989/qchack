36558a8aa969208a63e8595b573c6c512775d240
Ian <icrossett9@gmail.com>
2021-04-10 19:07:25 +0000

    made a file

---
+++ OurGroup/Hacking.py
@@ -0,0 +1,30 @@
+import numpy as np
+from qiskit import *
+
+circ = QuantumCircuit(3, 3)
+
+circ.h(0)
+# Add a CX (CNOT) gate on control qubit 0 and target qubit 1, putting
+# the qubits in a Bell state.
+circ.cx(0, 1)
+# Add a CX (CNOT) gate on control qubit 0 and target qubit 2, putting
+# the qubits in a GHZ state.
+circ.cx(0, 2)
+
+circ.draw('mpl')
+
+circ.measure(0,1)
+
+# compile the circuit down to low-level QASM instructions
+# supported by the backend (not needed for simple circuits)
+compiled_circuit = transpile(circuit, simulator)
+
+# Execute the circuit on the qasm simulator
+job = simulator.run(compiled_circuit, shots=1000)
+
+# Grab results from the job
+result = job.result()
+
+# Returns counts
+counts = result.get_counts(circuit)
+print("\nTotal count for 00 and 11 are:",counts)
